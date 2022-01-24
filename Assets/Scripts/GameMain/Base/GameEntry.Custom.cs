9a0dfdf7ab037cf4d2e345af38b10beed0335ff3
dreamCirno <dreamcirno9@foxmail.com>
2022-01-24 14:32:08 +0800

    新增游戏入口

---
+++ Assets/Scripts/GameMain/Base/GameEntry.Custom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace {
+    /// <summary>
+    /// 游戏入口。
+    /// </summary>
+    public partial class GameEntry : MonoBehaviour {
+        private static void InitCustomComponents() {
+            
+        }
+    }
+}
