423119ebdbc88b6a5cfda1806d5d3e1d7c685bda
dreamCirno <dreamcirno9@foxmail.com>
2022-01-27 16:47:08 +0800

    新增 TextImporter

---
+++ Assets/Scripts/CirnoFramework/Editor/Importer/TextImporter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace CirnoFramework.Editor.Importer {
+    [UnityEditor.AssetImporters.ScriptedImporter(1, new string[] {"lua", "pb", "proto"})]
+    public class TextImporter : UnityEditor.AssetImporters.ScriptedImporter {
+        public override void OnImportAsset(UnityEditor.AssetImporters.AssetImportContext ctx) {
+            var asset = new TextAsset(File.ReadAllText(ctx.assetPath));
+            ctx.AddObjectToAsset("MainText", asset);
+            ctx.SetMainObject(asset);
+        }
+    }
+}
