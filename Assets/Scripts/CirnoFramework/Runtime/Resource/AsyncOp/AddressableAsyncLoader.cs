ca7c07f01bf15cd5fec9b6616494d8b93b1b8ead
dreamCirno <dreamcirno9@foxmail.com>
2022-02-09 11:28:48 +0800

    新增 ResourceAsyncOpeartion、AddressableAsyncLoader

---
+++ Assets/Scripts/CirnoFramework/Runtime/Resource/AsyncOp/AddressableAsyncLoader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using CirnoFramework.Runtime.Utility;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using XLua;
+using Object = UnityEngine.Object;
+
+namespace CirnoFramework.Runtime.Resource.AsyncOp {
+    [LuaCallCSharp]
+    public class AddressableAsyncLoader : BaseAssetAsyncLoader {
+        private static readonly Queue<AddressableAsyncLoader> Pool = new Queue<AddressableAsyncLoader>();
+        private static int _sequence = 0;
+
+        public int Sequence { get; protected set; }
+        public Type AssetType { get; protected set; }
+        public string AddressPath { get; protected set; }
+
+        protected bool IsOver = false;
+
+        private dynamic _handle;
+
+        public static AddressableAsyncLoader Get() {
+            return Pool.Count > 0 ? Pool.Dequeue() : new AddressableAsyncLoader(++_sequence);
+        }
+
+        public static void Recycle(AddressableAsyncLoader creator) {
+            Pool.Enqueue(creator);
+        }
+
+        public AddressableAsyncLoader(int sequence) {
+            Sequence = sequence;
+        }
+
+        public void Init<T>(string addressPath, Type assetType) where T : Object {
+            Asset = null;
+            IsOver = false;
+            AssetType = assetType;
+            AddressPath = addressPath;
+
+            _handle = Addressables.LoadAssetAsync<T>(addressPath);
+        }
+
+        public override void Update() {
+            if (isDone) return;
+
+            _handle.Status = "123";
+
+            //if (_handle.Status == AsyncOperationStatus.Succeeded) {
+            //    Asset = _handle.Result as Object;
+            //    IsOver = true;
+            //}
+
+            //if (_handle.Status == AsyncOperationStatus.Failed) {
+            //    Log.Error($"Load asset:{AddressPath} error: {_handle.Status}");
+            //    IsOver = true;
+            //}
+        }
+
+        protected override bool IsDone() {
+            return IsOver;
+        }
+
+        protected override float Progress() {
+            return isDone ? 1.0f : 0.0f;
+        }
+
+        public override void Dispose() {
+            Recycle(this);
+        }
+    }
+}
