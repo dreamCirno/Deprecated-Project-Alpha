59b10e9119c35a9dbc5f3fe216eff80db7fd8559
dreamCirno <dreamcirno9@foxmail.com>
2022-02-16 16:43:15 +0800

    新增闪屏流程

---
+++ Assets/Scripts/GameMain/Procedure/Impl/ProcedureSplash.cs
@@ -0,0 +1,11 @@
+﻿using GameFramework.Fsm;
+using GameFramework.Procedure;
+using ProcedureBase = GameMain.Procedure.Base.ProcedureBase;
+
+namespace GameMain.Procedure.Impl {
+    public class ProcedureSplash : ProcedureBase {
+        protected override void OnEnter(IFsm<IProcedureManager> procedureOwner) {
+            base.OnEnter(procedureOwner);
+        }
+    }
+}
