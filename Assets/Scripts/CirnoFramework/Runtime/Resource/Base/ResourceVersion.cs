b6336d9f5d3d6866470c75ff362cb884e68e6882
dreamCirno <dreamcirno9@foxmail.com>
2022-02-16 15:39:06 +0800

    ResourceVersion 移除不必要的注释

--- Assets/Scripts/CirnoFramework/Runtime/Resource/Base/ResourceVersion.cs
+++ Assets/Scripts/CirnoFramework/Runtime/Resource/Base/ResourceVersion.cs
@@ -13,15 +13,5 @@ namespace CirnoFramework.Runtime.Resource.Base {
 
         public abstract void UpdateResource(Action<float, double, double, float> callback, Action downloadComplete,
             Action<string, string> errorCallback, string label = null);
-
-
-        // public abstract Task Initialize();
-        //
-        // public abstract Task UpdateCatalog(Action callback, Action<string> errorCallback);
-        //
-        // public abstract void CheckUpdate(Action<bool> needUpdate);
-        //
-        // public abstract void UpdateResource(Action<float, double, double, float> callback, Action downloadComplete,
-        //     Action<string, string> errorCallback, string key = null);
     }
 }
